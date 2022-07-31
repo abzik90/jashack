1047296581f06a1f57886fefa593c88f7805f6d8
Nurbol <nurbol.bizhigit@nu.edu.kz>
2022-07-31 09:38:41 +0600

    v0.0.1

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-HOST = "localhost"
-DB_NAME = "jashack"
-DB_USERNAME = "root"
-DB_PASSWORD = "root"
+HOST = "eu-cdbr-west-03.cleardb.net"
+DB_NAME = "heroku_e359f56e1a6fbc7"
+DB_USERNAME = "b01a8288052644"
+DB_PASSWORD = "e41bde02"
+
+# HOST = "localhost"
+# DB_NAME = "jashack"
+# DB_USERNAME = "root"
+# DB_PASSWORD = "root"
